06efc04fb27b580f91bc2a80222fe59c61977f8a
Marcelo Guzman Aguirre <27mga2003@gmail.com>
2025-02-09 19:21:13 -0500

    Changes

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -5,29 +5,27 @@ import pandas as pd
 df = pd.read_csv("data/Air_Quality.csv")
 data = df
 
-# Set up the Title and description
-st.title("NYC Air Quality Database Explorer")
+# Seting up app's title and description
+st.title("NYC Air Quality Database")
 st.write("This app allows you to explore NYC Air Quality data with interactive filtering options.")
 
-# Interactive filtering options
-# Dropdown for Name (air quality indicators)
+# Dropdown Name (air quality indicators)
 name = st.selectbox("Select Air Quality Indicator:", options=["All"] + list(data["Name"].unique()))
 
-# Filter data based on Name selection
+# Filtering data based on Name selection
 filtered_data = data if name == "All" else data[data["Name"] == name]
 
-# Dropdown for Geo Place Name (locations)
+# Dropdown for locations
 locations = st.selectbox("Select Location:", options=["All"] + list(filtered_data["Geo Place Name"].unique()))
 
-# Filter data based on location selection
+# Filtering data based on location selection
 filtered_data = filtered_data if locations == "All" else filtered_data[filtered_data["Geo Place Name"] == locations]
 
-# Time Period filter - exclude Annual Averages
-time_periods = sorted([period for period in filtered_data["Time Period"].unique() 
-                      if "Annual Average" not in str(period)])
+# Filtering time period
+time_periods = sorted(list(filtered_data["Time Period"].unique()))
 selected_time_period = st.selectbox("Select Time Period:", options=["All"] + time_periods)
 
-# Filter data based on time period selection
+# Filtering data based on time period selection
 filtered_data = filtered_data if selected_time_period == "All" else filtered_data[filtered_data["Time Period"] == selected_time_period]
 
 # Data Value range slider
@@ -43,6 +41,6 @@ filtered_data = filtered_data[
    (filtered_data["Data Value"] <= data_value_range[1])
 ]
 
-# Display filtered data and count
+# Displaying filtered data and count
 st.subheader(f"There are {len(filtered_data)} measurements that match your specifications")
 st.dataframe(filtered_data)
